65f4488977362a66960593de6627884529d6d4dd
Susha-123 <111412425+Susha-123@users.noreply.github.com>
2024-04-16 11:23:23 +0530

    Update main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 resource "aws_instance" "abhishek" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-080e1f13689e07408" # change this
+  subnet_id = "subnet-0d650db24e1ec20b5" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "susha-s3-demo" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
